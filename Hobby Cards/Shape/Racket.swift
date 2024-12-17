a90fe62677d0fa28400ee216cc38224f0c7ac721
Kevin Chen <kchen@lcs.on.ca>
2024-12-17 18:39:20 -0500

    Created Pingpong Racket

--- Hobby Cards/Shape/Racket.swift
+++ Hobby Cards/Shape/Racket.swift
@@ -11,21 +11,14 @@ struct Racket: Shape {
         var path = Path()
         path.move(to: CGPoint(x: rect.maxX*0.45, y: rect.maxY))
         path.addLine(to: CGPoint(x: rect.maxX*0.45, y: rect.maxY*0.75))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY*0.75))
-        path.addQuadCurve(to: CGPoint(x: rect.midX, y: rect.maxY*0.25), control: CGPoint(x: rect.maxX*0.95, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.maxX*0.55, y: rect.maxY*0.75))
+        path.addQuadCurve(to: CGPoint(x: rect.maxX*0.55, y: rect.maxY*0.25), control: CGPoint(x: rect.maxX*0.95, y: rect.midY))
         path.addLine(to: CGPoint(x: rect.maxX*0.45, y: rect.maxY*0.25))
-        path.addQuadCurve(to: CGPoint(x: rect.maxX*0.45, y: rect.maxY*0.75), control: CGPoint(x: rect.minX, y: rect.midY))
-        
-//        path.addQuadCurve(
-//            to: CGPoint(x: rect.midX, y: rect.maxY*0.25),
-//            control: CGPoint(x: rect.minX, y: rect.midY)
-//        )
-//        path.addQuadCurve(
-//            to: CGPoint(x: rect.midX, y: rect.maxY*0.25),
-//            control: CGPoint(x: rect.minX, y: rect.midY)
-//        )
-        
-        
+        path.addQuadCurve(to: CGPoint(x: rect.maxX*0.45, y: rect.maxY*0.75), control: CGPoint(x: rect.maxX*0.05, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.maxX*0.55, y: rect.maxY*0.75))
+        path.addLine(to: CGPoint(x: rect.maxX*0.55, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX*0.45, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX*0.45, y: rect.maxY*0.8))
         
         return path
     }
@@ -33,6 +26,6 @@ struct Racket: Shape {
 
 
 #Preview {
-    RacketView()
+    Racket()
         .aspectRatio(1.0/1.0, contentMode: .fit)
 }
