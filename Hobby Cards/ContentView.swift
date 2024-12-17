04dfca75f96dd45ffe4b193a2a42a90476df02db
Kevin Chen <kchen@lcs.on.ca>
2024-12-17 09:28:15 -0500

    Initial Commit

---
+++ Hobby Cards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hobby Cards
+//
+//  Created by Haowen Chen on 2024-12-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hobby Cards/Hobby_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hobby_CardsApp.swift
+//  Hobby Cards
+//
+//  Created by Haowen Chen on 2024-12-17.
+//
+
+import SwiftUI
+
+@main
+struct Hobby_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
